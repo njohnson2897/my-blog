f0161ba40d34b9fa63f66c682a82f3c77bafb124
Nate Johnson <njohnson2897@gmail.com>
2024-03-04 14:52:06 -0600

    adds pseudocode to javascript files

---
+++ assets/js/blog.js
@@ -0,0 +1,14 @@
+// a toggle button for light/dark functionality
+    // a query-selector for a toggle button in the HTML
+    // an event listener for clicks on the toggle
+    // if statements for switching between light and dark modes
+
+// a series of blog posts sorted into cards
+    // a getItem method for grabbing the user input data from the form
+    // JSON parsing for the getItem to change the data into a usable object
+    // logic for automatically populating the blog cards with the parsed user input data
+
+// back button
+    // a query-selector for the back button in HTML
+    // an event listener for clicks on the button
+    // a link to the form submission page attached to the button
---
+++ assets/js/form.js
@@ -0,0 +1,7 @@
+// submit button
+    // query-selector for the button in the HTML
+    // JSON.stringify to turn the user input information into a string from the object
+    // localStorage.setItem logic to store the stringified user input data
+    // preventDefault functionality to stop the submit button from simply refreshing the page
+    // a link in the submit button to take the user to the submissions page
+    // some if statements that return errors if the fields are not given the proper inputs
