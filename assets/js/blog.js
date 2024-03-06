ece9ad9ec27635cdc9b9e8e49326b4d376c56c0a
Nate Johnson <njohnson2897@gmail.com>
2024-03-05 19:33:18 -0600

    adds footer to both HTMLs and creates event listener for the toggle button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -2,6 +2,20 @@
     // a query-selector for a toggle button in the HTML
     // an event listener for clicks on the toggle
     // if statements for switching between light and dark modes
+const toggleSlider = document.querySelector('#toggle-slider')
+
+let mode = 'light';
+
+toggleSlider.addEventListener('click', function {
+    if (mode === 'light') {
+        mode = 'dark';
+        container.setAttribute('class', 'light');
+    }
+    else {
+        mode = 'dark';
+        container.setAttribute('class', 'dark')
+    }
+})
 
 // a series of blog posts sorted into cards
     // a getItem method for grabbing the user input data from the form
